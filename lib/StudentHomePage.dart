15e2f231404b8909b72a8e959d7e9f52a1f9dd52
HessaMoha <hessaalajaleen@gmail.com>
2024-10-12 23:00:45 +0300

    Student Home page and Training provider Home page (Hessa)

--- lib/StudentHomePage.dart
+++ lib/StudentHomePage.dart
@@ -1,14 +1,293 @@
 import 'package:flutter/material.dart';
 
-class StudentHomePage extends StatelessWidget {
+void main() {
+  runApp(HadafiApp());
+}
+
+class HadafiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hadafi App',
+      theme: ThemeData(
+        primaryColor: Color(0xFF096499),
+        scaffoldBackgroundColor: Color(0xFFF3F9FB),
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
+
+class HadafiDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Color(0xFFF3F9FB),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFF096499),
+              ),
+              child: Image.asset(
+                'assets/images/LOGO.png',
+                fit: BoxFit.contain,
+                height: 80,
+              ),
+            ),
+            _buildDrawerItem(context, Icons.person, 'Profile', ProfilePage()),
+            _buildDrawerItem(context, Icons.home, 'Home', HomePage()),
+            _buildDrawerItem(context, Icons.assignment, 'CV Enhancement Tool', null),
+            _buildDrawerItem(context, Icons.chat, 'Interview Simulator', null),
+            _buildDrawerItem(context, Icons.feedback, 'Feedback', null),
+            _buildDrawerItem(context, Icons.group, 'Communities', null),
+            _buildDrawerItem(context, Icons.favorite, 'Favorites List', null), // Favorites List button added here
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.logout, color: Color(0xFF2F83C5)),
+              title: Text('Logout'),
+              onTap: () {
+                Navigator.pop(context);
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text('Logged out')));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDrawerItem(BuildContext context, IconData icon, String title, Widget? page) {
+    return ListTile(
+      leading: Icon(icon, color: Color(0xFF2F83C5)),
+      title: Text(title),
+      onTap: () {
+        Navigator.pop(context);
+        if (page != null) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => page),
+          );
+        }
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: HadafiDrawer(),
       appBar: AppBar(
-        title: Text('Student Home'),
+        backgroundColor: Color(0xFF096499),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(1),
+            child: Image.asset(
+              'assets/images/LOGO.png',
+              fit: BoxFit.contain,
+              height: 300,
+            ),
+          ),
+        ],
+        iconTheme: IconThemeData(color: Colors.white),
       ),
-      body: Center(
-        child: Text('Welcome to the Student Dashboard!'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _buildSectionTitle('Training Opportunities'),
+            _buildOpportunitiesTab(),
+            _buildSectionTitle('Feedback from Users'),
+            _buildFeedbackList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF096499),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOpportunitiesTab() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      child: Container(
+        height: 490,
+        child: DefaultTabController(
+          length: 2,
+          child: Column(
+            children: [
+              TabBar(
+                labelColor: Color(0xFF096499),
+                unselectedLabelColor: Colors.grey,
+                indicatorColor: Color(0xFF096499),
+                tabs: [
+                  Tab(text: 'Best Match'),
+                  Tab(text: 'Other'),
+                ],
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    OpportunitiesList(title: 'Best Match Opportunities'),
+                    OpportunitiesList(title: 'Other Opportunities'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFeedbackList() {
+    return Container(
+      height: 150,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Container(
+            width: 200,
+            margin: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: Color(0xFF096499), width: 2),
+              boxShadow: [
+                BoxShadow(color: Colors.black12, blurRadius: 5, spreadRadius: 2),
+              ],
+            ),
+            child: ListTile(
+              title: Text('User ${index + 1}'),
+              subtitle: Text('This app helped me find a great internship!'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class OpportunitiesList extends StatelessWidget {
+  final String title;
+
+  OpportunitiesList({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Color(0xFF096499), width: 2),
+              boxShadow: [
+                BoxShadow(color: Colors.black12, blurRadius: 5, spreadRadius: 2),
+              ],
+            ),
+            child: ListTile(
+              title: Text('$title ${index + 1}'),
+              subtitle: Text('Based on your qualifications'),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final TextEditingController _nameController = TextEditingController(text: "Jane Doe");
+  final TextEditingController _emailController = TextEditingController(text: "jane.doe@example.com");
+  final TextEditingController _gpaController = TextEditingController(text: "3.8");
+  final TextEditingController _majorController = TextEditingController(text: "Software Engineering");
+  final TextEditingController _skillsController = TextEditingController(text: "Dart, Flutter, Java");
+  final TextEditingController _certificatesController = TextEditingController(text: "AWS Certified Developer");
+  final TextEditingController _locationController = TextEditingController(text: "City, Country");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: HadafiDrawer(),
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor: Color(0xFF096499),
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Profile saved')),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            _buildEditableField('Student Name', _nameController),
+            _buildEditableField('Email', _emailController),
+            _buildEditableField('GPA', _gpaController),
+            _buildEditableField('Major', _majorController),
+            _buildEditableField('Skills', _skillsController),
+            _buildEditableField('Certificates', _certificatesController),
+            _buildEditableField('Location', _locationController),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEditableField(String label, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
       ),
     );
   }
--- lib/TrainingProviderHomePage.dart
+++ lib/TrainingProviderHomePage.dart
@@ -1,189 +1,294 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-class TrainingProviderHomePage extends StatefulWidget {
-  const TrainingProviderHomePage({Key? key}) : super(key: key);
-
-  @override
-  _TrainingProviderHomePageState createState() =>
-      _TrainingProviderHomePageState();
+void main() {
+  runApp(TrainingProviderApp());
 }
 
-class _TrainingProviderHomePageState extends State<TrainingProviderHomePage> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  String? _userId;
-  bool _isLoading = false;
-
+class TrainingProviderApp extends StatelessWidget {
   @override
-  void initState() {
-    super.initState();
-    _userId = _auth.currentUser?.uid; // Get the current logged-in user's ID
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Training Provider App',
+      theme: ThemeData(
+        primaryColor: Color(0xFF096499),
+        scaffoldBackgroundColor: Color(0xFFF3F9FB),
+      ),
+      home: TrainingProviderHomePage(), // Default home page for training provider
+    );
   }
+}
 
-  Future<void> _addJob() async {
-    TextEditingController titleController = TextEditingController();
-    TextEditingController descriptionController = TextEditingController();
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Add New Job'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: titleController,
-                decoration: const InputDecoration(
-                  labelText: 'Job Title',
-                ),
-              ),
-              TextField(
-                controller: descriptionController,
-                decoration: const InputDecoration(
-                  labelText: 'Job Description',
-                ),
-              ),
-            ],
+class TrainingProviderHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF096499),
+        iconTheme: IconThemeData(color: Colors.white), // White menu button
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Image.asset(
+              'assets/images/LOGO.png', // Path to the logo
+              fit: BoxFit.contain,
+              height: 50, // Adjust logo size
+            ),
           ),
-          actions: [
-            TextButton(
-              onPressed: () async {
-                await _firestore
-                    .collection('TrainingProvider')
-                    .doc(_userId)
-                    .collection('Jobs')
-                    .add({
-                  'title': titleController.text,
-                  'description': descriptionController.text,
-                  'created_at': FieldValue.serverTimestamp(),
-                });
-                Navigator.of(context).pop();
+        ],
+      ),
+      drawer: _buildDrawer(context),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF096499),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              ),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Navigate to Post New Opportunity page'),
+                ));
               },
-              child: const Text('Add'),
+              child: Text(
+                'Post New Opportunity',
+                style: TextStyle(color: Colors.white), // Set text color to white
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Posted Opportunities',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF096499),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5, // Example number of posted opportunities
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color: Color(0xFF096499), width: 2),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5,
+                            spreadRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: ListTile(
+                        title: Text('Opportunity ${index + 1}'),
+                        subtitle: Text('Details about opportunity ${index + 1}'),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('Deleted Opportunity ${index + 1}'),
+                            ));
+                          },
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
-        );
-      },
+        ),
+      ),
     );
   }
 
-  Future<void> _deleteJob(String jobId) async {
-    await _firestore
-        .collection('TrainingProvider')
-        .doc(_userId)
-        .collection('Jobs')
-        .doc(jobId)
-        .delete();
-  }
-
-  Future<void> _editJob(
-      String jobId, String currentTitle, String currentDescription) async {
-    TextEditingController titleController =
-        TextEditingController(text: currentTitle);
-    TextEditingController descriptionController =
-        TextEditingController(text: currentDescription);
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Edit Job'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: titleController,
-                decoration: const InputDecoration(
-                  labelText: 'Job Title',
-                ),
+  Widget _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Color(0xFFF3F9FB),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFF096499),
               ),
-              TextField(
-                controller: descriptionController,
-                decoration: const InputDecoration(
-                  labelText: 'Job Description',
-                ),
+              child: Image.asset(
+                'assets/images/LOGO.png',
+                fit: BoxFit.contain,
+                height: 80,
               ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () async {
-                await _firestore
-                    .collection('TrainingProvider')
-                    .doc(_userId)
-                    .collection('Jobs')
-                    .doc(jobId)
-                    .update({
-                  'title': titleController.text,
-                  'description': descriptionController.text,
-                });
-                Navigator.of(context).pop();
+            ),
+            ListTile(
+              leading: Icon(Icons.person, color: Color(0xFF2F83C5)), // Profile icon
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProviderProfilePage()),
+                );
               },
-              child: const Text('Save Changes'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Color(0xFF2F83C5)), // Home icon
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.logout, color: Color(0xFF2F83C5)),
+              title: Text('Log Out'),
+              onTap: () {
+                Navigator.pop(context);
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Logged Out'),
+                ));
+              },
             ),
           ],
-        );
-      },
+        ),
+      ),
     );
   }
+}
+
+class ProviderProfilePage extends StatefulWidget {
+  @override
+  _ProviderProfilePageState createState() => _ProviderProfilePageState();
+}
+
+class _ProviderProfilePageState extends State<ProviderProfilePage> {
+  // Variables to hold editable fields
+  String companyName = 'Example Corp.';
+  String email = 'example@corp.com';
+  String companyLocation = 'Riyadh, Saudi Arabia';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Training Provider Home'),
-        backgroundColor: const Color(0xFF113F67),
+        title: Text('Profile'),
+        backgroundColor: Color(0xFF096499), // AppBar color set to match theme
+        iconTheme: IconThemeData(color: Colors.white), // White icon color
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save, color: Colors.white), // Save icon color to white
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('Profile Updated'),
+              ));
+            },
+          ),
+        ],
       ),
-      body: _userId == null
-          ? const Center(child: Text('No user logged in'))
-          : StreamBuilder<QuerySnapshot>(
-              stream: _firestore
-                  .collection('TrainingProvider')
-                  .doc(_userId)
-                  .collection('Jobs')
-                  .orderBy('created_at', descending: true)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (_isLoading) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                if (!snapshot.hasData) {
-                  return const Center(child: Text('No job postings found.'));
-                }
-                var jobs = snapshot.data!.docs;
-                return ListView.builder(
-                  itemCount: jobs.length,
-                  itemBuilder: (context, index) {
-                    var job = jobs[index];
-                    return ListTile(
-                      title: Text(job['title']),
-                      subtitle: Text(job['description']),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.edit),
-                            onPressed: () => _editJob(
-                                job.id, job['title'], job['description']),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () => _deleteJob(job.id),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+      drawer: _buildDrawer(context),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildEditableProfileField('Company Name:', companyName, (value) {
+              setState(() {
+                companyName = value;
+              });
+            }),
+            SizedBox(height: 16), // Consistent spacing
+            _buildEditableProfileField('Email:', email, (value) {
+              setState(() {
+                email = value;
+              });
+            }),
+            SizedBox(height: 16), // Consistent spacing
+            _buildEditableProfileField('Company Location:', companyLocation, (value) {
+              setState(() {
+                companyLocation = value;
+              });
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Color(0xFFF3F9FB),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFF096499),
+              ),
+              child: Image.asset(
+                'assets/images/LOGO.png',
+                fit: BoxFit.contain,
+                height: 80,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person, color: Color(0xFF2F83C5)), // Profile icon
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Color(0xFF2F83C5)), // Home icon
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TrainingProviderHomePage()),
                 );
               },
             ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addJob,
-        backgroundColor: const Color(0xFF113F67),
-        child: const Icon(Icons.add),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.logout, color: Color(0xFF2F83C5)),
+              title: Text('Log Out'),
+              onTap: () {
+                Navigator.pop(context);
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Logged Out'),
+                ));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEditableProfileField(String label, String initialValue, ValueChanged<String> onChanged) {
+    TextEditingController controller = TextEditingController(text: initialValue);
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8.0), // Add vertical margin for spacing
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12), // Consistent border radius
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF096499), width: 2.0),
+            borderRadius: BorderRadius.circular(12), // Consistent border radius
+          ),
+        ),
+        onChanged: onChanged,
       ),
     );
   }
