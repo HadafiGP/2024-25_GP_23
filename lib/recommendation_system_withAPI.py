befe495593364bc23462fb4375ec5c3dd82dcea2
Aljawhara Mansour Abdullah <144268551+Aljawhara-Aldhayan-443200433@users.noreply.github.com>
2025-05-03 19:00:34 +0300

    fix

--- lib/recommendation_system_withAPI.py
+++ lib/recommendation_system_withAPI.py
@@ -38,7 +38,7 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 # Load COOP/internship opportunities dataset
-dataset_name = "LinkedInOpportunities.csv.csv"
+dataset_name = "LinkedInOpportunities.csv"
 dataset_path = os.path.join(script_dir, dataset_name)
 
 # Handle dataset errors
